26e5d58fbe701ac9a271062e79413a657bb3587c
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-05-09 09:31:49 +0800

    chore: fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,7 @@
 import { sxzz } from '@sxzz/eslint-config'
-export default sxzz([
-  {
-    files: ['README.md/*.ts'],
-    rules: {
-      'import/no-mutable-exports': 'off',
-    },
+export default sxzz().append({
+  files: ['README.md/*.ts'],
+  rules: {
+    'import/no-mutable-exports': 'off',
   },
-])
+})
